d5c0ea1a982a2ac612b5b4f464e9a58f390e8dfa
deucalioncodes <jmpmonteagudo@gmail.com>
2026-02-27 19:55:06 +0100

    fix: await fetchRootKey in counter test to fix certificate verification on PocketIC

--- examples/counter/test/test.ts
+++ examples/counter/test/test.ts
@@ -1,11 +1,17 @@
 import { createSnakeCaseProxy, getCanisterId, runTests } from 'azle/test';
 import { get_tests as getTests } from 'azle/examples/counter/test/tests';
 import { createActor } from './dfx_generated/counter';
+import { HttpAgent } from '@dfinity/agent';
 
-const counterCanister = createActor(getCanisterId('counter'), {
-    agentOptions: {
-        host: 'http://127.0.0.1:8000'
-    }
-});
+async function main() {
+    const agent = new HttpAgent({ host: 'http://127.0.0.1:8000' });
+    await agent.fetchRootKey();
 
-runTests(getTests(createSnakeCaseProxy(counterCanister)));
+    const counterCanister = createActor(getCanisterId('counter'), {
+        agent
+    });
+
+    runTests(getTests(createSnakeCaseProxy(counterCanister)));
+}
+
+main();
