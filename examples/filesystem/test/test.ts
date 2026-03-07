eef335515697ea260e04b6f940aa7bddc247733e
deucalioncodes <jmpmonteagudo@gmail.com>
2026-03-07 21:26:21 +0100

    Update filesystem test to use HttpAgent with fetchRootKey; simplify CI test command

--- examples/filesystem/test/test.ts
+++ examples/filesystem/test/test.ts
@@ -1,11 +1,17 @@
 import { getCanisterId, runTests } from 'azle/test';
 import { createActor } from './dfx_generated/filesystem';
 import { getTests } from './tests';
+import { HttpAgent } from '@dfinity/agent';
 
-const filesystemCanister = createActor(getCanisterId('filesystem'), {
-    agentOptions: {
-        host: 'http://127.0.0.1:8000'
-    }
-});
+async function main() {
+    const agent = new HttpAgent({ host: 'http://127.0.0.1:8000' });
+    await agent.fetchRootKey();
 
-runTests(getTests(filesystemCanister));
+    const filesystemCanister = createActor(getCanisterId('filesystem'), {
+        agent
+    });
+
+    runTests(getTests(filesystemCanister));
+}
+
+main();
